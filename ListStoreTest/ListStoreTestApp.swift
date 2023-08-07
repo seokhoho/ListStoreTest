fe7206b5cbc0696b1b9635429793b10cf2a26f3c
Seokho Jeong <kreel9402@gmail.com>
2023-08-07 20:11:14 +0900

    Initial Commit

---
+++ ListStoreTest/ListStoreTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListStoreTestApp.swift
+//  ListStoreTest
+//
+//  Created by 정유진 on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct ListStoreTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
